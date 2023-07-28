4fc5ae9b7521fc20d13396c563a8fd7f2beb15b6
Chertushkin Roman <chertushkinroman@gmail.com>
2023-07-28 09:39:28 +0400

    Added heaader to backpack modal

---
+++ src/components/BackPack/BackpackHeader.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="backpack-header">
+    <div class="backpack-header__tab backpack_tab active">BACKPACK</div>
+    <div class="backpack-header__tab nexus_tab inactive">NEXUS</div>
+    <div class="backpack-header__tab close_tab active">
+        <img src="@/assets/icons/times.svg" alt="close" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.backpack-header {
+  width: 100%;
+  height: 59px;
+  display: flex;
+}
+
+.backpack-header__tab {
+    border: 1px solid #000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 24px;
+    font-style: normal;
+    font-family: "Archivo";
+    font-weight: 400;
+    line-height: normal;
+    text-transform: uppercase;
+}
+
+.backpack_tab {
+width: 249px;
+}
+
+.nexus_tab {
+width: 257px;
+}
+
+.close_tab {
+width: 56px;
+}
+
+.active {
+  background: #242223;
+  color: #FFFFFF
+}
+
+.inactive {
+  background: #393839;
+  color: rgba(255, 255, 255, 0.47)
+}
+
+</style>
---
+++ src/components/BackPack/BackpackModal.vue
@@ -0,0 +1,25 @@
+<template>
+<div class='backpack-modal'>
+  <BackpackHeader />
+
+  <div>
+  
+  </div>
+
+</div>
+</template>
+
+<script setup lang="ts">
+import BackpackHeader from './BackpackHeader.vue'
+</script>
+
+<style scoped>
+.backpack-modal {
+  width: 100%;
+  height: 100%;
+  width: 562px;
+  height: 864px;
+  flex-shrink: 0;
+  background: red
+}
+</style>
